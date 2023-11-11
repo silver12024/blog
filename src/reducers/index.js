f9e1736a1da512c6dbf634e91b99557616120576
silver12024 <83531553+silver12024@users.noreply.github.com>
2023-11-11 07:36:19 +0700

    Delete src directory

--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from 'redux' 
-import {postReducer} from './postReducer';
-import commentReducer from './commentReducer';
-
-const reducers =  combineReducers({
-    postReducer,
-    comments: commentReducer,       
-});
-export default reducers;
